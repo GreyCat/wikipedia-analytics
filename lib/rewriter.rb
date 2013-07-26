e815145c381bc48fbb1cc91dc5e39862650c4712
Mikhail Yakshin <greycat@altlinux.org>
2013-07-26 16:54:35 +0400

    Better logging; updated summary message with a clear reference to bot name

--- lib/rewriter.rb
+++ lib/rewriter.rb
@@ -15,31 +15,33 @@ class Rewriter
 		File.open(list).each_line { |title|
 			title.chomp!
 			t = title.gsub(/ /, '_')
-			@log.info "#{t} - #{grabber.class.to_s} - processing"
+			prefix = "#{grabber.class.to_s.gsub(/^.*::/, '')} - #{t} - "
+
+			@log.info "#{prefix}processing"
 
 			src = @wc.get(t)
-			@log.info "#{t} - #{grabber.class.to_s} - downloaded Wikipedia article"
+			@log.info "#{prefix}downloaded Wikipedia article"
 
 			begin
 				dest = processor.process(src)
 			rescue Processor::NoDataFromGrabber
-				@log.warn "#{t} - #{grabber.class.to_s} - got no data from external web analytics system"
+				@log.warn "#{prefix}got no data from external web analytics system"
 				next
 			rescue Processor::NoTemplateFound
-				@log.warn "#{t} - #{grabber.class.to_s} - unable to match & update template"
+				@log.warn "#{prefix}unable to match & update template"
 				next
 			end
 
 			if src != dest
 				confirmed = @options[:auto] ? true : manual_confirmation(src, dest)
 				if confirmed
-					@wc.edit(t, dest, :summary => "Актуализирована #{tmpl}", :minor => 1)
-					@log.info "#{t} - #{grabber.class.to_s} - updated Wikipedia article"
+					@wc.edit(t, dest, :summary => "[[Участник:WebAnalyticsBot|Web Analytics Bot]]: актуализирована #{tmpl}", :minor => 1)
+					@log.info "#{prefix}updated Wikipedia article"
 				else
-					@log.warn "#{t} - #{grabber.class.to_s} - modification not confirmed"
+					@log.warn "#{prefix}modification not confirmed"
 				end
 			else
-				@log.warn "#{t} - #{grabber.class.to_s} - not modified"
+				@log.warn "#{prefix}not modified"
 			end
 		}
 	end
